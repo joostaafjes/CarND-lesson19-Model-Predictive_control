bb0c4beeb933ac1cef3cfd54229dfa5e0dc20fc0
joost aafjes <joostaafjes@gmail.com>
2019-01-26 15:48:30 +0100

    first version with basic cost function

---
+++ MPC.h
@@ -0,0 +1,22 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  std::string getN();
+  std::string getDt();
+
+  // Solve the model given an initial state.
+  // Return the next state and actuations as a vector.
+  std::vector<double> Solve(const Eigen::VectorXd &x0, 
+                            const Eigen::VectorXd &coeffs);
+};
+
+#endif  // MPC_H
